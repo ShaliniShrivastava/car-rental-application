0e45e78e949ba61ffbd320fcd83f061bc66afa0a
ShaliniShrivastava <sha.shri21@gmail.com>
2026-03-10 15:34:15 +0530

    ok

--- SERVERcar/app.js
+++ SERVERcar/app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://car-rental-with-suraj.netlify.app"
+    "https://radiant-llama-3e88b6.netlify.app"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
